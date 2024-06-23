5cf2b6b0639d8d9c62800e1b872f6f6d1535528e
mansiichoudharii <101002318+mansiichoudharii@users.noreply.github.com>
2024-06-23 11:37:06 +0530

    variables

---
+++ basics/variables_01.js
@@ -0,0 +1,6 @@
+const accountId = 12345;
+let accountEmail = "mansi@gmail.com";
+var accountPassword = "1442233";
+accountStatus = 123
+
+console.table(accountEmail, accountId, accountPassword, accountStatus);
