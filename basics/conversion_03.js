0f5adf2acca69d0ad882acbd338ae9a3747b884d
mansiichoudharii <101002318+mansiichoudharii@users.noreply.github.com>
2024-06-23 12:10:14 +0530

    conversion

---
+++ basics/conversion_03.js
@@ -0,0 +1,12 @@
+let score = true;
+
+// console.log(typeof score);
+
+let valueInNum = Number(score);
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn)
